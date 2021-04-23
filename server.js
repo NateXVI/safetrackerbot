7a3ac51846032d233edae3ff4a3fcc21e770af67
Nate Diven <natediven1@gmail.com>
2021-04-23 11:32:34 -0600

    Needs tidying

--- server.js
+++ server.js
@@ -25,15 +25,15 @@ client.on('message', async (message) => {
         switch(text[0].toLowerCase()) {
             case 'price':
             case 'p':
-                const p = await getPrice();
-                message.channel.send(p)
+                const p = await firstRes();
+                message.channel.send(`$${p.usdLast}\nSource: ${p.exchangeName}`)
                 break;
             case 'get':
             case 'g':
                 const v = await getValue(message.author.id);
                 // console.log(v);
                 if (v) {
-                    message.channel.send(`Your **${numberWithCommas(users[message.author.id])}** safemoon is worth **$${numberWithCommas(v)}!**`)
+                    message.channel.send(`Your **${numberWithCommas(users[message.author.id])}** safemoon is worth **$${numberWithCommas(v.value)}!**\nSource: ${v.exchangeName}`)
                 } else {
                     message.channel.send('tell me how much safemoon you have by using the **set** command')
                 }
@@ -66,15 +66,18 @@ async function getPrice() {
 }
 
 async function setStatus() {
-    client.user.setPresence({activity: {name: `$${await getPrice()}`}});
+    let info = await firstRes();
+    client.user.setPresence({activity: {name: `$${info.usdLast}`}});
+    console.log(await firstRes().close)
     setTimeout(setStatus, 1 * 60000)
     console.log('Status set')
 }
 
 async function getValue(id) {
     if (!!!users[id]) return false;
-    const p = await getPrice();
-    return p * Number(users[id].replace(/\,/g, ''));
+    let p = await firstRes();
+    p.value = p.usdLast * Number(users[id].replace(/\,/g, ''));
+    return p;
 }
 
 function setQuantity(id, quantity) {
@@ -95,3 +98,21 @@ function checkString(s) {
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
 }
+
+async function getData() {
+    const req = await axios.get('https://api.cryptorank.io/v0/coins/safemoon/tickers');
+    return req.data.data;
+}
+
+async function firstRes() {
+    let req = await axios.get('https://api.cryptorank.io/v0/coins/safemoon/tickers');
+    req = req.data.data;
+    const i = req.findIndex((v) => v.exchangeName == 'Pancake Swap');
+    // console.log(i, req[i]);
+
+    return req[i] || req[0];
+}
+
+(async function() {
+    await firstRes();
+})();
