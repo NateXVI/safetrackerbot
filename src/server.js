eb449da4a644ebf5e14cbb3ea6dc651dccaa46fb
Nate Diven <natediven1@gmail.com>
2021-04-30 03:59:13 -0600

    changes file structure and added dockerfile

--- src/server.js
+++ src/server.js
@@ -30,27 +30,35 @@ client.on('ready', () => {
     }, 60000, client)
 });
 
+// when the bot recieves a message
 client.on('message', (message) => {
+    // get the content of the message
     let text = message.content;
 
+    // if the first character in the message is the prefix
     if (text.charAt(0) == config.prefix) {
         text = text.slice(config.prefix.length);
         text = text.split(' ');
 
+        // convert text to lowercase to ignore case
         switch(text[0].toLowerCase()) {
+            // if help send help message
             case 'help':
             case 'h':
                 bot.help(message, config);
                 break;
+            // if price send back the price and how much the user's value
             case 'price':
             case 'p':
                 console.log(users[message.author.id])
                 bot.price(message, users[message.author.id]);
                 break;
+            // if set, set the user's value
             case 'set':
             case 's':
                 bot.set(message, users, message.author.id, text[1]);
                 break;
+            // if rank rank the different exchanges that are available
             case 'rank':
             case 'r':
                 bot.rank(message)
@@ -59,7 +67,8 @@ client.on('message', (message) => {
     }
 });
 
+// login to the discord bot
 client.login(token);
 
-// safe.getList().then((val) => console.log(val));
+
 
