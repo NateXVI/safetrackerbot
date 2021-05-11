8cfa4d2580fd15624d952b9c134c7bc61d6cd78c
NateXVI <natediven1@gmail.com>
2021-05-11 01:41:52 -0600

    If pancake swap isn't found, return the first result

--- src/SafeMoonAPI.js
+++ src/SafeMoonAPI.js
@@ -1,19 +1,21 @@
-const { default: axios } = require("axios");
+const { default: axios } = require('axios');
 
 class SafeAPI {
-    url = 'https://api.cryptorank.io/v0/coins/safemoon/tickers'
-    async getList() {
-        const res = await axios('https://api.cryptorank.io/v0/coins/safemoon/tickers')
-        return res.data.data;
-    }
-    async getOne() {
-        let res = await axios('https://api.cryptorank.io/v0/coins/safemoon/tickers');
-        res = res.data.data;
-        const i = res.findIndex((v) => v.exchangeName == 'Pancake Swap');
-
-        return res[i];
-    }
-    
+	url = 'https://api.cryptorank.io/v0/coins/safemoon/tickers';
+	async getList() {
+		const res = await axios(
+			'https://api.cryptorank.io/v0/coins/safemoon/tickers'
+		);
+		return res.data.data;
+	}
+	async getOne() {
+		let res = await axios(
+			'https://api.cryptorank.io/v0/coins/safemoon/tickers'
+		);
+		res = res.data.data;
+		const i = res.findIndex((v) => v.exchangeName == 'Pancake Swap');
+		return res[i] || res[0];
+	}
 }
 
 module.exports = SafeAPI;
