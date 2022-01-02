6af26d6e1263afd65796bbf9f998ef74190b4faa
Nate <natediven1@gmail.com>
2022-01-02 06:02:40 -0700

    updated for sfm v2

--- src/SafeMoonAPI.js
+++ src/SafeMoonAPI.js
@@ -1,17 +1,13 @@
 const { default: axios } = require('axios');
 
 class SafeAPI {
-	url = 'https://api.cryptorank.io/v0/coins/safemoon/tickers';
+	url = 'https://api.cryptorank.io/v0/coins/safemoon-v2/tickers';
 	async getList() {
-		const res = await axios(
-			'https://api.cryptorank.io/v0/coins/safemoon/tickers'
-		);
+		const res = await axios('https://api.cryptorank.io/v0/coins/safemoon-v2/tickers');
 		return res.data.data;
 	}
 	async getOne() {
-		let res = await axios(
-			'https://api.cryptorank.io/v0/coins/safemoon/tickers'
-		);
+		let res = await axios('https://api.cryptorank.io/v0/coins/safemoon-v2/tickers');
 		res = res.data.data;
 		const i = res.findIndex((v) => v.exchangeName == 'Pancake Swap');
 		return res[i] || res[0];
